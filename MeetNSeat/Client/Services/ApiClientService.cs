368a14ed9d1eee22be62197b89b795ecec5b62ed
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2021-04-23 13:28:00 +0200

    IpChecker added to client

---
+++ MeetNSeat/Client/Services/ApiClientService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using MeetNSeat.Client.Models;
+
+namespace MeetNSeat.Client.Services
+{
+    public class ApiClientService : IApiClientService
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public ApiClientService(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IpAddress> GetUserIPAsync()
+        {
+            var client = httpClientFactory.CreateClient("IP");
+            return await client.GetFromJsonAsync<IpAddress>("/");
+        }
+    }
+}
