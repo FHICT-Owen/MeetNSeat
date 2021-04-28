104ffbaff5fbd539d35d7e66549e664f8ca19737
Revol <33750267+Revol963@users.noreply.github.com>
2021-04-28 15:48:19 +0200

    Feedback CRUD

---
+++ MeetNSeat/MeetNSeat.Dal/FeedbackDal.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Dapper;
+using MeetNSeat.Dal.Interfaces;
+
+namespace MeetNSeat.Dal
+{
+    public class FeedbackDal : IFeedbackDal
+    {
+        public bool DeleteFeedback(int feedbackID)
+        {
+            using IDbConnection connection = new SqlConnection(Connection.GetConnectionString("DefaultConnection"));
+            if (connection.ExecuteScalar<bool>("EXEC DeleteFeedback", feedbackID, commandType: CommandType.StoredProcedure))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<FeedbackDto> getAllFeedbacks()
+        {
+            using IDbConnection connection = new SqlConnection(Connection.GetConnectionString("DefaultConnection"));
+            var query = connection.Query<FeedbackDto>("EXEC GetAllFeedback", commandType: CommandType.StoredProcedure);
+            List<FeedbackDto> feedbacks = query.ToList();
+            return feedbacks;
+        }
+        public FeedbackDto GetFeedbackDtoByID(int feedbackID)
+        {
+            using IDbConnection connection = new SqlConnection(Connection.GetConnectionString("DefaultConnection"));
+            FeedbackDto feedbackDto = connection.ExecuteScalar<FeedbackDto>("EXEC GetFeedbackByID", feedbackID);
+            return feedbackDto;
+        }
+        public bool InsertFeedback(FeedbackDto feedbackDto)
+        {
+
+            using IDbConnection connection = new SqlConnection(Connection.GetConnectionString("DefaultConnection"));
+            DynamicParameters parameter = new DynamicParameters();
+
+            parameter.Add("@Description", feedbackDto.FeedbackID);
+            parameter.Add("@FeedbackState", feedbackDto.FeedbackState);
+            //TODO: If statement
+            if (connection.ExecuteScalar<bool>("EXEC InsertFeedback", parameter))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateFeedback(FeedbackDto feedbackDto)
+        {
+            using IDbConnection connection = new SqlConnection(Connection.GetConnectionString("DefaultConnection"));
+            if (connection.ExecuteScalar<bool>("EXEC UpdateFeedback", feedbackDto))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
