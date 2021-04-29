5b0bd535512fee422f4d6a6a9ef3c568665fd8ed
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2021-04-29 10:47:48 +0200

    Added sessionStorage

--- MeetNSeat/Client/Program.cs
+++ MeetNSeat/Client/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazored.SessionStorage;
 
 namespace MeetNSeat.Client
 {
@@ -18,6 +19,7 @@ namespace MeetNSeat.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            builder.Services.AddBlazoredSessionStorage();
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddOidcAuthentication(options =>
             {
