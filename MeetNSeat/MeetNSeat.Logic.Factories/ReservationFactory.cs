681e699820435fbb110d3ab12cd65fb802867c80
DirkLemmen <33750270+DirkLemmen@users.noreply.github.com>
2021-04-29 11:18:30 +0200

    Add reservation front-end
    Connection string not working

---
+++ MeetNSeat/MeetNSeat.Logic.Factories/ReservationFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MeetNSeat.Logic.Interfaces;
+
+namespace MeetNSeat.Logic.Factories
+{
+    public class ReservationFactory
+    {
+        public static IManageReservation AddReservation()
+        {
+            return new User();
+        }
+    }
+}
