fb127921dc8706f8dac09f1ff3c6fb1c88860381
Siem Lucassen <siem228@hotmail.com>
2021-04-30 13:20:43 +0200

    Mijn oude changes verwijderd

---
+++ MeetNSeat/Server/Controllers/ReservationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeetNSeat.Logic;
+using MeetNSeat.Logic.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace MeetNSeat.Server.Controllers
+{
+    public class ReservationController
+    {
+        private IManageReservation _manageReservation;
+
+        public ReservationController(IManageReservation manageReservation)
+        {
+            _manageReservation = manageReservation;
+        }
+
+        [HttpPost("api")]
+        public void CreateReservation()
+        {
+            _manageReservation.AddReservation(1, 1, 1, DateTime.Now, DateTime.Now);
+        }
+
+        //manageReservation = ReservationFactory.AddReservation();
+        //manageReservation.AddReservation(1, 1, 1, DateTime.Now, DateTime.Now);
+    }
+}
