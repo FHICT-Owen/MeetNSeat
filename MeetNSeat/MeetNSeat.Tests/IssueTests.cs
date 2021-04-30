1667bd8e2f06eb463d189743661fd17ad77120fd
Schellingerhout,Koen K <k.schellingerhout@student.fontys.nl>
2021-04-30 13:37:49 +0200

    Changed issue test names

--- MeetNSeat/MeetNSeat.Tests/IssueTests.cs
+++ MeetNSeat/MeetNSeat.Tests/IssueTests.cs
@@ -8,23 +8,11 @@ namespace MeetNSeat.Tests
 	public class UnitTest1
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void ReportNewIssue()
 		{
-			var r = new IssueCollection();
+			var issueCollection = new IssueCollection();
 
-			r.AddIssue(1, "s1", "YOtest");
-		}
-	}
-	
-	[TestClass]
-	public class UnitTest2
-	{
-		[TestMethod]
-		public void TestMethod1()
-		{
-			var r = new User();
-
-			//r.AddReservation(1, "1",5, DateTime.Now, DateTime.Now);
+			issueCollection.AddIssue(1, "test", "test");
 		}
 	}
 }
