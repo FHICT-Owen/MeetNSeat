8321de45595121490a96c22ce2a0ad2532488a9a
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2021-05-06 11:02:18 +0200

    Changed dbsettings

--- MeetNSeat/MeetNSeat.Dal/Connection.cs
+++ MeetNSeat/MeetNSeat.Dal/Connection.cs
@@ -1,14 +1,16 @@
-﻿namespace MeetNSeat.Dal
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace MeetNSeat.Dal
 {
     public static class Connection
     {
         public static string GetConnectionString(string name)
         {
-            /*IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("./dbSettings.json")
-                .Build();*/
-            return "Data Source=cgi-wradbserver.database.windows.net;Initial Catalog=CGI-WRA_db;Persist Security Info=True;User ID=grotebaas;Password=Grote7856";
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("dbSettings.json")
+                .Build();
+            return configuration.GetConnectionString("DefaultConnection");
         }
     }
 }
