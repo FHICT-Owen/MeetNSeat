6c77744f5b7dc4248be8d87f9c33d1bc5d758810
Siem Lucassen <siem228@hotmail.com>
2021-05-20 15:35:22 +0200

    Added Floor Interface

--- MeetNSeat/MeetNSeat.Dal.Factories/FloorFactory.cs
+++ MeetNSeat/MeetNSeat.Dal.Factories/FloorFactory.cs
@@ -1,7 +1,12 @@
+using MeetNSeat.Dal.Interfaces;
+
 namespace MeetNSeat.Dal.Factories
 {
     public class FloorFactory
     {
-        
+        public static IFloorDal CreateFloorDal()
+        {
+            return new FloorDal();
+        }
     }
 }
--- MeetNSeat/MeetNSeat.Logic/Floor.cs
+++ MeetNSeat/MeetNSeat.Logic/Floor.cs
@@ -1,6 +1,26 @@
-﻿namespace MeetNSeat.Logic
+﻿using System.Collections.Generic;
+using MeetNSeat.Dal.Factories;
+using MeetNSeat.Dal.Interfaces;
+using MeetNSeat.Dal.Interfaces.Dtos;
+
+namespace MeetNSeat.Logic
 {
     public class Floor
     {
+        private readonly IFloorDal _dal;
+
+        public int Id { get; set; }
+        public int LocationId { get; set; }
+        public string Name { get; set; }
+
+        public Floor()
+        {
+            _dal = FloorFactory.CreateFloorDal();
+        }
+
+        public IReadOnlyCollection<FloorDto> GetAllFloorsByLocation(int locationId)
+        {
+            return _dal.GetAllFloorsByLocation(locationId);
+        }
     }
 }
