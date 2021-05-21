b3999c0a75ff40c1acf16de860acc56e9706e2c7
Siem Lucassen <siem228@hotmail.com>
2021-05-22 01:10:52 +0200

    Added MudBlazorComponents

--- MeetNSeat/Client/Program.cs
+++ MeetNSeat/Client/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using MudBlazor.Services;
 
 namespace MeetNSeat.Client
 {
@@ -16,6 +17,7 @@ namespace MeetNSeat.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.Services.AddBlazoredSessionStorage();
+            builder.Services.AddMudServices();
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddOidcAuthentication(options =>
             {
