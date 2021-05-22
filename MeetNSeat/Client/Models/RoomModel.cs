cf6302fa49402a535c8cce3ff961e7c98cd0c3c8
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-05-22 17:12:35 +0200

    Fixed RoomModel

--- MeetNSeat/Client/Models/RoomModel.cs
+++ MeetNSeat/Client/Models/RoomModel.cs
@@ -4,14 +4,16 @@
     {
         public int Id { get; set; }
         public int FloorId { get; set; }
+        public string Name { get; set; }
         public string Type { get; set; }
         public int Spots { get; set; }
         public string Facilities { get; set; }
 
-        public RoomModel(int id, int floorId, string type, int spots, string facilities)
+        public RoomModel(int id, int floorId, string name, string type, int spots, string facilities)
         {
             Id = id;
             FloorId = floorId;
+            Name = name;
             Type = type;
             Spots = spots;
             Facilities = facilities;
