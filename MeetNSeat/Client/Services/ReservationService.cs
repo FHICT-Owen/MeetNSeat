52daf1afb76f94b58ed52ce299463493eb334ab2
Revol <33750267+Revol963@users.noreply.github.com>
2021-05-27 14:38:53 +0200

    Vrijgeven debug

--- MeetNSeat/Client/Services/ReservationService.cs
+++ MeetNSeat/Client/Services/ReservationService.cs
@@ -8,7 +8,7 @@ namespace MeetNSeat.Client.Services
         public static async Task<string> DeleteReservation(int id)
         {
             using var client = new HttpClient();
-            var msg = await client.DeleteAsync("https://localhost:5001/api/reservation/" + id);
+            var msg = await client.DeleteAsync("https://localhost:5001/api/user/" + id);
             if (msg.IsSuccessStatusCode) return "Your reservation has been canceled";
             return "Something went wrong :( Please try again later";
         }
