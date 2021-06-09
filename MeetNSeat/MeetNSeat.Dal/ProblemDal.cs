e834fb4abcfa2a102f700e137cf51aa93d41a5e4
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-06-09 16:53:08 +0200

    minor changes

--- MeetNSeat/MeetNSeat.Dal/ProblemDal.cs
+++ MeetNSeat/MeetNSeat.Dal/ProblemDal.cs
@@ -13,31 +13,31 @@ namespace MeetNSeat.Dal
 	{
 		public List<ProblemDto> GetAllProblems()
 		{
-            using IDbConnection connection = new SqlConnection(Connection.GetConnectionString());
-				var output = connection.Query<ProblemDto>("dbo.GetAllProblems").ToList();
-				return output;
-        }
+			using IDbConnection connection = new SqlConnection(Connection.GetConnectionString());
+			var output = connection.Query<ProblemDto>("dbo.GetAllProblems").ToList();
+			return output;
+		}
 
 		public void AddProblem(ProblemDto problem)
 		{
-            using IDbConnection connection = new SqlConnection(Connection.GetConnectionString());
-            connection.Execute("dbo.InsertProblem @RoomId, @UserId, @Email, @Description, @Picture, @ReportedOn, @IsResolved, @ResolvedAt", problem);
-        }
+			using IDbConnection connection = new SqlConnection(Connection.GetConnectionString());
+			connection.Execute("dbo.InsertProblem @RoomId, @UserId, @Email, @Description, @Picture, @ReportedOn, @IsResolved, @ResolvedAt", problem);
+		}
 
 		public void DeleteProblemById(int id)
 		{
-            using IDbConnection connection = new SqlConnection(Connection.GetConnectionString());
-			
-				var parameters = new DynamicParameters();
-				parameters.Add("@Id", id);
-				
-				connection.Execute("dbo.DeleteProblemById @Id", parameters);
-        }
-		
+			using IDbConnection connection = new SqlConnection(Connection.GetConnectionString());
+
+			var parameters = new DynamicParameters();
+			parameters.Add("@Id", id);
+
+			connection.Execute("dbo.DeleteProblemById @Id", parameters);
+		}
+
 		public void UpdateProblem(ProblemDto problem)
 		{
-            using IDbConnection connection = new SqlConnection(Connection.GetConnectionString());
-            connection.Execute("dbo.UpdateProblem @Id, @RoomId, @UserId, @Email, @Description, @Picture, @IsResolved, @ResolvedAt", problem);
-        }
+			using IDbConnection connection = new SqlConnection(Connection.GetConnectionString());
+			connection.Execute("dbo.UpdateProblem @Id, @RoomId, @UserId, @Email, @Description, @Picture, @IsResolved, @ResolvedAt", problem);
+		}
 	}
 }
